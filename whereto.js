e13d790df36463a519924a03c6c8a02035df3b58
JazzSteve <63267630+JazzSteve@users.noreply.github.com>
2020-05-10 14:26:03 -0400

    finished contact section and footer

--- whereto.js
+++ whereto.js
@@ -9,19 +9,19 @@ $(function(){
 
 
 // Navbar scroll change height
-$(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 50) {
-	    $(".mainnavbar").css("height", "90px");
-	    $(".mainnavbar").css("transition", "height 500ms");
-	    $("#search-button").css("top", "20px");
-	  }
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//   	var scroll = $(window).scrollTop();
+// 	  if (scroll > 50) {
+// 	    $(".mainnavbar").css("height", "90px");
+// 	    $(".mainnavbar").css("transition", "height 500ms");
+// 	    $("#search-button").css("top", "20px");
+// 	  }
 
-	  else{
-		  $(".mainnavbar").css("height", "116px");
-		  $("#search-button").css("top", "30px");
-		  $("#search-button").css("transition", "top 500ms");	
-	  }
-  });
-});
+// 	  else{
+// 		  $(".mainnavbar").css("height", "116px");
+// 		  $("#search-button").css("top", "30px");
+// 		  $("#search-button").css("transition", "top 500ms");	
+// 	  }
+//   });
+// });
